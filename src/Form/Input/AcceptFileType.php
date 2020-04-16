8e60d78af18e0fe27353c13633b5472c58be6696
nemundo <urs.lang@gmail.com>
2020-04-16 08:59:39 +0200

    .

--- src/Form/Input/AcceptFileType.php
+++ src/Form/Input/AcceptFileType.php
@@ -8,6 +8,13 @@ class AcceptFileType
 
     const IMAGE = 'image/*';
 
+//    const WEB_IMAGE = 'image/x-png,image/gif,image/jpeg';
+    const WEB_IMAGE = 'image/png,image/gif,image/jpeg';
+
+
+    // image/x-png,image/gif,image/jpeg
+
+
     const VIDEO = 'video/*';
 
     const AUDIO = 'audio/*';
