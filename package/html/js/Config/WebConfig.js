9e539a38aafec90bf541aa9ebacba9c63678df6b
Lang Urs <urs.lang@luzern.com>
2024-02-14 22:42:53 +0100

    .

--- package/html/js/Config/WebConfig.js
+++ package/html/js/Config/WebConfig.js
@@ -2,10 +2,13 @@ export default class WebConfig {
 
     static webUrl = '/';
 
-
-    static loadCurrentWeb() {
+    static loadCurrentWeb(language = null) {
 
         WebConfig.webUrl = (new URL(import.meta.url)).origin + "/";
+
+        if (language !== null) {
+            WebConfig.webUrl = WebConfig.webUrl + language + "/";
+        }
 
     }
 
